1fb812d5a9cc1b1a320139677ad604ab5dfc22c0
tarangsachaniya <tarangsachaniya8@gmail.com>
2024-06-26 15:14:34 +0530

    middleware added.

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,19 @@
 import dotenv from "dotenv";
 import expr from "express";
 import connectDb from "./helper/index.js";
-
+import cors from 'cors';
 const app = expr();
 dotenv.config({
-      path: "./.env",
-    });
-    
+  path: "./.env",
+});
+
+app.use(expr.json());
+
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
+
 connectDb()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
